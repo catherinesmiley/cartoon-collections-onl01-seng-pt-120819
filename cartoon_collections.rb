f9ca0db3f0ae54bf8f7e98735792a306359a7874
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 02:47:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -49,5 +49,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find {|cheese| 
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
