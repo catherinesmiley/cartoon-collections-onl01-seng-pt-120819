e0dc436583dd334a20203ce19f2ef6c913aea328
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 02:34:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,39 @@
-def roll_call_dwarves(names)
-  names.each_with_index do |name, index|
-    puts "#{index+1} #{name}"
-  end
-end
+# def roll_call_dwarves(names)
+#   names.each_with_index do |name, index|
+#     puts "#{index+1} #{name}"
+#   end
+# end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect { |call| call.capitalize + "!"}
-end
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.collect { |call| call.capitalize + "!"}
+# end
 
-def long_planeteer_calls(calls_long)
-  calls_long.any?{|i| i.length > 4}
-end
+# def long_planeteer_calls(calls_long)
+#   calls_long.any?{|i| i.length > 4}
+# end
 
-def find_the_cheese(cheddar_cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.find do |cheese|
-    cheese_types.include?(cheese)
-  end
-end
+# def find_the_cheese(cheddar_cheese)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheddar_cheese.find do |cheese|
+#     cheese_types.include?(cheese)
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
